d6409411f774214d6b258902f31bb4a36b487d4b
wakemaster39 <wakemaster39@gmail.com>
2011-05-25 16:55:21 -0400

    Added ability to do rails initializer style to control threading.

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -7,12 +7,13 @@ require 'logger'
 
 module Delayed
   class Worker
-    cattr_accessor :min_priority, :max_priority, :max_attempts, :max_run_time, :default_priority, :sleep_delay, :logger, :delay_jobs
+    cattr_accessor :min_priority, :max_priority, :max_attempts, :max_run_time, :default_priority, :sleep_delay, :logger, :delay_jobs, :threaded
     self.sleep_delay = 5
     self.max_attempts = 25
     self.max_run_time = 4.hours
     self.default_priority = 0
     self.delay_jobs = true
+    self.threaded = false
 
     # By default failed jobs are destroyed after too many attempts. If you want to keep them around
     # (perhaps to inspect the reason for the failure), set this to false.
@@ -50,7 +51,8 @@ module Delayed
       self.class.min_priority = options[:min_priority] if options.has_key?(:min_priority)
       self.class.max_priority = options[:max_priority] if options.has_key?(:max_priority)
       self.class.sleep_delay = options[:sleep_delay] if options.has_key?(:sleep_delay)
-      @cant_fork = true unless options[:threaded]
+      self.class.threaded = options[:threaded] if options.has_key?(:threaded)
+      @cant_fork = true unless self.class.threaded
     end
 
     # Every worker has a unique name which by default is the pid of the process. There are some
