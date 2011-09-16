24ffecc5cbdbbfeeaf2750862a89bc35f01f8d43
ziggy1 <matveyev.dmitry@gmail.com>
2011-09-16 16:48:24 +0400

    Edited lib/delayed/serialization/active_record.rb via GitHub

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -2,13 +2,21 @@ class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
   def self.yaml_new(klass, tag, val)
-    if ActiveRecord::VERSION::MAJOR == 3
-      klass.unscoped.find(val['attributes'][klass.primary_key])
-    else # Rails 2
-      klass.with_exclusive_scope { klass.find(val['attributes'][klass.primary_key]) }
-    end
+    klass.unscoped.find(val['attributes'][klass.primary_key])
   rescue ActiveRecord::RecordNotFound
-    raise Delayed::DeserializationError
+    foo = klass.new
+    val['attributes'].each do |k, v|
+      foo[k] = v
+    end
+    foo
+  end
+  
+  def persistent?
+    primary_key = self['attributes'][self.class.primary_key]
+    if primary_key_value = self['attributes'][primary_key]
+      obj = self.class.where(primary_key.to_sym => primary_key_value).select(primary_key).first
+      obj ? true : false
+    end
   end
 
   def to_yaml_properties
