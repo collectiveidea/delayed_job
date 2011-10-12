040e9c92cf866b716fc9ebf6baafa6d8997d26f9
Dmitry <matveyev.dmitry@gmail.com>
2011-10-12 09:45:51 +0400

    Commenting out 'yaml_new' method. It doesn't work if uploading yamled objects from another database

--- lib/delayed/serialization/active_record.rb
+++ lib/delayed/serialization/active_record.rb
@@ -1,15 +1,16 @@
 class ActiveRecord::Base
   yaml_as "tag:ruby.yaml.org,2002:ActiveRecord"
 
-  def self.yaml_new(klass, tag, val)
-    klass.unscoped.find(val['attributes'][klass.primary_key])
-  rescue ActiveRecord::RecordNotFound
-    foo = klass.new
-    val['attributes'].each do |k, v|
-      foo[k] = v
-    end
-    foo
-  end
+# This doesn't work if uploading yamled objects from another database
+# def self.yaml_new(klass, tag, val)
+#    klass.unscoped.find(val['attributes'][klass.primary_key])
+#  rescue ActiveRecord::RecordNotFound
+#    foo = klass.new
+#    val['attributes'].each do |k, v|
+#      foo[k] = v
+#    end
+#    foo
+#  end
   
   def persistent?
     primary_key = self['attributes'][self.class.primary_key]
