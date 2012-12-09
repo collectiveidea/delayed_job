f55b1a2dd740e9e942b7294bcff07193f6b87828
Wil Gieseler <supapuerco@gmail.com>
2012-12-09 14:16:36 -0800

    rake jobs:work_once

--- lib/delayed/tasks.rb
+++ lib/delayed/tasks.rb
@@ -8,4 +8,13 @@ namespace :jobs do
   task :work => :environment do
     Delayed::Worker.new(:min_priority => ENV['MIN_PRIORITY'], :max_priority => ENV['MAX_PRIORITY'], :queues => (ENV['QUEUES'] || ENV['QUEUE'] || '').split(','), :quiet => false).start
   end
+
+  desc "Works all jobs in the queue (if any) and exits."
+  task :work_once => :environment do
+    job_count = Delayed::Job.count
+    puts "There are #{job_count} jobs in the queue."
+    result = Delayed::Job.work_off job_count
+    puts "#{result.sum} jobs processed: #{result.first} succeeded, #{result.last} failed." 
+  end
+
 end
