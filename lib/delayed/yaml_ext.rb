dcb8c40993a17425891885d4c0c3d014d2f318d9
Alvaro <alvaro.perez.shirley@gmail.com>
2014-02-19 16:42:08 -0800

    Remove sych

--- lib/delayed/yaml_ext.rb
+++ lib/delayed/yaml_ext.rb
@@ -2,7 +2,6 @@
 # Classes, Modules and Structs
 
 require 'yaml'
-YAML::ENGINE.yamler = "syck" if defined?(YAML::ENGINE)
 if YAML.parser.class.name =~ /syck|yecht/i
   require File.expand_path('../syck_ext', __FILE__)
   require File.expand_path('../serialization/active_record', __FILE__)
