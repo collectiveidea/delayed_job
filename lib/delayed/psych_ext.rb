Betterment/delayed

commit a47e0d7b55a30fb5853f20b139caddd39535727f

Don't override AR yaml generation

lib/delayed/psych_ext.rb
@@ -1,22 +1,3 @@
-if defined?(ActiveRecord)
-  ActiveRecord::Base.class_eval do
-    # rubocop:disable BlockNesting
-    if instance_methods.include?(:encode_with)
-      def encode_with_override(coder)
-        encode_with_without_override(coder)
-        coder.tag = "!ruby/ActiveRecord:#{self.class.name}" if coder.respond_to?(:tag=)
-      end
-      alias_method :encode_with_without_override, :encode_with
-      alias_method :encode_with, :encode_with_override
-    else
-      def encode_with(coder)
-        coder['attributes'] = attributes
-        coder.tag = "!ruby/ActiveRecord:#{self.class.name}" if coder.respond_to?(:tag=)
-      end
-    end
-  end
-end
-
 module Delayed
   class PerformableMethod
     # serialize to YAML
@@ -46,10 +27,21 @@ module Delayed
         end
       end
 
-      def visit_Psych_Nodes_Mapping(object) # rubocop:disable PerceivedComplexity, CyclomaticComplexity, MethodName
+      def visit_Psych_Nodes_Mapping(object) # rubocop:disable CyclomaticComplexity, MethodName, PerceivedComplexity
         return revive(Psych.load_tags[object.tag], object) if Psych.load_tags[object.tag]
 
         case object.tag
+        when /^!ruby\/object/
+          result = super
+          if defined?(ActiveRecord::Base) && result.is_a?(ActiveRecord::Base)
+            begin
+              result.class.find(result[result.class.primary_key])
+            rescue ActiveRecord::RecordNotFound => error # rubocop:disable BlockNesting
+              raise Delayed::DeserializationError, "ActiveRecord::RecordNotFound, class: #{klass}, primary key: #{id} (#{error.message})"
+            end
+          else
+            result
+          end
         when /^!ruby\/ActiveRecord:(.+)$/
           klass = resolve_class(Regexp.last_match[1])
           payload = Hash[*object.children.map { |c| accept c }]

spec/helper.rb
@@ -23,6 +23,9 @@ require 'delayed/backend/shared_spec'
 Delayed::Worker.logger = Logger.new('/tmp/dj.log')
 ENV['RAILS_ENV'] = 'test'
 
+# Trigger AR to initialize
+ActiveRecord::Base # rubocop:disable Void
+
 module Rails
   def self.root
     '.'

collectiveidea/delayed_job

commit 274611a11f03a6969aaaea5572cb269b7c28d177

Added missing 'unscoped' (broken in commit a47e0d7b55a30fb5853f20b139caddd39535727f), which is needed for delayed_jobs with default-scoped ActiveRecord arguments to function correctly (= as they used to in v.4.0.3). The error message in the rescue block is also broken, but this seems to be fixed in another pull request.

lib/delayed/psych_ext.rb
@@ -35,7 +35,7 @@ module Delayed
           result = super
           if defined?(ActiveRecord::Base) && result.is_a?(ActiveRecord::Base)
             begin
-              result.class.find(result[result.class.primary_key])
+              result.class.unscoped.find(result[result.class.primary_key])
             rescue ActiveRecord::RecordNotFound => error # rubocop:disable BlockNesting
               raise Delayed::DeserializationError, "ActiveRecord::RecordNotFound, class: #{klass}, primary key: #{id} (#{error.message})"
             end