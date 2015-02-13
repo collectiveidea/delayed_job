96d21e93d15cac5f3ef8e5242dcca3385d5a4c66
shyam-habarakada <shyam.habarakada@gmail.com>
2015-02-13 15:19:18 -0800

    bug fix

--- lib/delayed/command.rb
+++ lib/delayed/command.rb
@@ -119,7 +119,7 @@ module Delayed
       Delayed::Worker.after_fork
       Delayed::Worker.logger ||= Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
       
-      ActiveRecord::Base.logger = Delayed::Worker.logger if Delayed::Worker.backend == :active_record
+      ActiveRecord::Base.logger = Delayed::Worker.logger if Delayed::Worker.backend.new.is_a? Delayed::Backend::ActiveRecord::Job
 
       worker = Delayed::Worker.new(options)
       worker.name_prefix = "#{worker_name} "
