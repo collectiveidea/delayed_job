04f5aa8877134d0a4cfeb2c4fcf1ec3e3d7a9686
Roderick Monje <rod@foveacentral.com>
2017-10-10 18:43:08 -0400

    Revert "fix!: lib/delayed/backend/shared_spec.rb:677"
    This reverts commit 4879a67f45ae0d46df2321201b1925a76bce10cb.

--- lib/delayed/backend/shared_spec.rb
+++ lib/delayed/backend/shared_spec.rb
@@ -674,11 +674,7 @@ shared_examples_for 'a delayed_job backend' do
         it_behaves_like 'any failure more than Worker.max_attempts times'
 
         it 'is not destroyed if it failed more than Worker.max_attempts times' do
-          if Delayed::Worker.destroy_failed_jobs
-            expect(@job).to receive(:destroy)
-          else
-            expect(@job).to receive(:destroy)
-          end
+          expect(@job).not_to receive(:destroy)
           Delayed::Worker.max_attempts.times { worker.reschedule(@job) }
         end
 
