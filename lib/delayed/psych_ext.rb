1ce33824cd346f5d8915c3ec33f7055fe39f45c3
Tony Novak <afn2@cornell.edu>
2018-01-02 13:14:05 -0500

    Catch more specific error rather than Exception
    ScriptError is the superclass of NotImplementedError, SyntaxError, and
    LoadError.

--- lib/delayed/psych_ext.rb
+++ lib/delayed/psych_ext.rb
@@ -76,7 +76,7 @@ module Delayed
         else
           begin
             super
-          rescue Exception => error
+          rescue ScriptError => error
             raise Delayed::DeserializationError, "#{error.class}, class: #{klass} (#{error.message})"
           end
         end
