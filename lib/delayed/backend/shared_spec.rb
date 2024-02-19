0469b0940fd51def20c258bc8f6c3afe1176d8f3
Orien Madgwick <497874+orien@users.noreply.github.com>
2024-02-19 21:18:35 +1100

    Loosen test expectation to match Ruby 3.4 backtrace

--- lib/delayed/backend/shared_spec.rb
+++ lib/delayed/backend/shared_spec.rb
@@ -595,7 +595,7 @@ shared_examples_for 'a delayed_job backend' do
         worker.work_off
         @job.reload
         expect(@job.last_error).to match(/did not work/)
-        expect(@job.last_error).to match(/sample_jobs.rb:\d+:in `perform'/)
+        expect(@job.last_error).to match(/sample_jobs.rb:\d+:in [`'](ErrorJob#)?perform'/)
         expect(@job.attempts).to eq(1)
         expect(@job.run_at).to be > Delayed::Job.db_time_now - 10.minutes
         expect(@job.run_at).to be < Delayed::Job.db_time_now + 10.minutes
