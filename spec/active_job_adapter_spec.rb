54153a19a7bcf1cc963684af78c77cbe99dcdea1
David Genord II <david@collectiveidea.com>
2025-07-09 13:13:31 -0400

    Apply rubocop updates

--- spec/active_job_adapter_spec.rb
+++ spec/active_job_adapter_spec.rb
@@ -29,7 +29,7 @@ describe 'a Rails active job backend' do
     end
   end
 
-  let(:worker) { Delayed::Worker.new(sleep_delay: 0.5, queues: %w[integration_tests]) }
+  let(:worker) { Delayed::Worker.new(:sleep_delay => 0.5, :queues => %w[integration_tests]) }
 
   before do
     JobBuffer.clear
@@ -38,7 +38,7 @@ describe 'a Rails active job backend' do
     ActiveJob::Base.logger = nil
   end
 
-  it "should supply a wrapped class name to DelayedJob" do
+  it 'should supply a wrapped class name to DelayedJob' do
     TestJob.perform_later
     job = Delayed::Job.all.last
     expect(job.name).to match(/TestJob \[[0-9a-f-]+\] from DelayedJob\(integration_tests\) with arguments: \[\]/)
@@ -52,13 +52,13 @@ describe 'a Rails active job backend' do
     end
   end
 
-  it "should not run jobs queued on a non-listening queue" do
+  it 'should not run jobs queued on a non-listening queue' do
     start_worker do
       old_queue = TestJob.queue_name
 
       begin
         TestJob.queue_as :some_other_queue
-        TestJob.perform_later "Rails"
+        TestJob.perform_later 'Rails'
         sleep 2
         expect(JobBuffer.values.empty?).to eq true
       ensure
@@ -71,13 +71,13 @@ describe 'a Rails active job backend' do
     start_worker do
       ActiveJob.perform_all_later(TestJob.new('Rails'), TestJob.new('World'))
       sleep 2
-      expect(JobBuffer.values).to eq(['Rails', 'World'])
+      expect(JobBuffer.values).to eq(%w[Rails World])
     end
   end
 
   it 'should not run job enqueued in the future' do
     start_worker do
-      TestJob.set(wait: 5.seconds).perform_later('Rails')
+      TestJob.set(:wait => 5.seconds).perform_later('Rails')
       sleep 2
       expect(JobBuffer.values.empty?).to eq true
     end
@@ -85,37 +85,38 @@ describe 'a Rails active job backend' do
 
   it 'should run job enqueued in the future at the specified time' do
     start_worker do
-      TestJob.set(wait: 5.seconds).perform_later('Rails')
+      TestJob.set(:wait => 5.seconds).perform_later('Rails')
       sleep 10
       expect(JobBuffer.values).to eq(['Rails'])
     end
   end
 
-  it "should run job bulk enqueued in the future at the specified time" do
+  it 'should run job bulk enqueued in the future at the specified time' do
     start_worker do
-      ActiveJob.perform_all_later([TestJob.new("Rails").set(wait: 5.seconds)])
+      ActiveJob.perform_all_later([TestJob.new('Rails').set(:wait => 5.seconds)])
       sleep 10
       expect(JobBuffer.values).to eq(['Rails'])
     end
   end
 
-  it "should run job with higher priority first" do
+  it 'should run job with higher priority first' do
     start_worker do
       wait_until = Time.now + 3.seconds
-      TestJob.set(wait_until: wait_until, priority: 20).perform_later "1"
-      TestJob.set(wait_until: wait_until, priority: 10).perform_later "2"
+      TestJob.set(:wait_until => wait_until, :priority => 20).perform_later '1'
+      TestJob.set(:wait_until => wait_until, :priority => 10).perform_later '2'
       sleep 10
 
-      expect(JobBuffer.values).to eq(['2', '1'])
+      expect(JobBuffer.values).to eq(%w[2 1])
     end
   end
 
   private
-    def start_worker(&)
-      thread = Thread.new { worker.start }
-      yield
-    ensure
-      worker.stop
-      thread.join
-    end
+
+  def start_worker
+    thread = Thread.new { worker.start }
+    yield
+  ensure
+    worker.stop
+    thread.join
+  end
 end
