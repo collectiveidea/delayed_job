17c923f81fed4611c1ae77e88e3337ed6d634b8e
johnnyshields <27655+johnnyshields@users.noreply.github.com>
2025-08-07 01:31:30 +0900

    Remove compatibility shim

--- lib/generators/delayed_job/delayed_job_generator.rb
+++ lib/generators/delayed_job/delayed_job_generator.rb
@@ -5,7 +5,7 @@ class DelayedJobGenerator < Rails::Generators::Base
   source_paths << File.join(File.dirname(__FILE__), 'templates')
 
   def create_executable_file
-    template 'script', "#{Delayed::Compatibility.executable_prefix}/delayed_job"
-    chmod "#{Delayed::Compatibility.executable_prefix}/delayed_job", 0o755
+    template 'script', 'bin/delayed_job'
+    chmod 'bin/delayed_job', 0o755
   end
 end
