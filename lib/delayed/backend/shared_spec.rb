dafbffc09fd44d9093edb5eb2a40468f863bfca0
David Genord II <david@collectiveidea.com>
2025-12-29 17:01:31 -0500

    Jruby 10 error syntax

--- lib/delayed/backend/shared_spec.rb
+++ lib/delayed/backend/shared_spec.rb
@@ -595,9 +595,14 @@ shared_examples_for 'a delayed_job backend' do
         worker.work_off
         @job.reload
         expect(@job.last_error).to match(/did not work/)
-        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.4.0') && !defined?(JRUBY_VERSION)
-          # Ruby 3.4 produces a more verbose message
-          expect(@job.last_error).to match(/sample_jobs.rb:\d+:in 'ErrorJob#perform'/)
+        if Gem::Version.new(RUBY_VERSION) >= Gem::Version.new('3.4.0')
+          if defined?(JRUBY_VERSION)
+            # Jruby partially adopted ruby's error syntax changes
+            expect(@job.last_error).to match(/sample_jobs.rb:\d+:in 'perform'/)
+          else
+            # Ruby 3.4 produces a more verbose message
+            expect(@job.last_error).to match(/sample_jobs.rb:\d+:in 'ErrorJob#perform'/)
+          end
         else
           expect(@job.last_error).to match(/sample_jobs.rb:\d+:in `perform'/)
         end
